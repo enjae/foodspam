638434102b90f1cc40f71e5a972ec25be156e86d
manavdhiman <65016580+ManavdhiMan003@users.noreply.github.com>
2021-06-08 16:25:57 +0530

    add feature of login and signup for user with password encryption

--- app.py
+++ app.py
@@ -1,10 +1,79 @@
-from flask import Flask,render_template,redirect
-
+from flask import Flask,render_template,redirect,session,request
+import db as api
+import foodspam_crypto as crypto
+import generate as gen
+import credential
 app = Flask(__name__)
+app.secret_key=credential.secret_key
 
 @app.route('/')
 def home():
+    if 'username' in session:
+        return render_template('index.html')
     return render_template('home.html')
+@app.route('/index')
+def index():
+    # if 'username' in session:
+    return render_template('index.html')
+
+@app.route('/user_login',methods=['POST','GET'])
+def user_login():
+    try:
+        if 'username' in session:
+            return render_template('index.html')
+        if request.method=='POST':
+            details = request.form
+            # print(details)
+            # print(details['password'])
+            # password = crypto.encrypt(details['password'])
+            # print(password,"     jhhhh")
+            t = api.user_login(details['username'],details['password'])
+            if(t==1):
+                session['username']=details['username']
+                return redirect('/index')
+            if t==0:
+                print("wrong password")
+                return render_template('/user_templates/login.html')
+            if t==-1:
+                print("username dosenot exist")
+                return render_template('/user_templates/login.html')    
+        return render_template("/user_templates/login.html")
+    except Exception as e:
+        print(e)
+        return render_template("/user_templates/login.html")
+# gAAAAABgv0gTwO65hoBGIKQQUFRCKZXSAh8g3F6zyXmJ19nPNLskHvtEFHLEYD5s0hLdi65E6gTwZJm-SRWV-YyNQ2-j6nMdMw==
+@app.route('/user_signup',methods=['POST','GET'])
+def user_signup():
+    try:
+        if 'username' in session:
+            return render_template('index.html')
+        if request.method=='POST':
+            details = request.form
+            # print(details)
+            id = gen.get_id()
+            id = "User"+str(id)
+            while(api.user_check(id)==1):
+                id = gen.get_id()
+                id = "User"+str(id) 
+                print("hello") 
+            password = crypto.encrypt(details['password'])
+            password = str(password,'UTF-8')
+            # print(password)
+            t = api.user_signup(id,details['name'],details['username'],password,details['email'],details['phone_no'])
+            if(t==1):
+                # session['username']=details['username']
+                return redirect('/user_login')
+            if t==0:
+                print("signup insertion error")
+                return render_template('/user_templates/signup.html')   
+        return render_template("/user_templates/signup.html")
+    except Exception as e:
+        print(e)
+        return render_template("/user_templates/signup.html")
+
+@app.route('/ngo_login')
+def ngo_login():
+    return render_template("//ngo_templates//ngo_login.html")
 
 if __name__=='__main__':
     app.run(debug=True)
--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 import pymysql
 import credential
+import foodspam_crypto as crypto
 
 def user_signup(id,name,username,password,email,phone_no):
     conn=pymysql.connect(
@@ -11,13 +12,14 @@ def user_signup(id,name,username,password,email,phone_no):
     )
     try:
         with conn.cursor() as curr:
-            sql = "insert into user_master (id,name,username,password,email,phone_no) value (%s,%s,%s,%s,%s,%s)"
+            sql = "insert into user_master (user_id,name,user_name,password,email,phone_no) value (%s,%s,%s,%s,%s,%s)"
             # phone_no=int(phone_no)
             curr.execute(sql,(id,name,username,password,email,phone_no))
             conn.commit()
+            return 1
     except Exception as e:
         print(e)        
-
+        return 0
 def user_login(username,password):
     conn=pymysql.connect(
         host=credential.host,
@@ -28,19 +30,24 @@ def user_login(username,password):
     )
     try:
         with conn.cursor() as curr:
-            sql = "select password from user_master where username=(%s)"
+            sql = "select password from user_master where user_name=(%s)"
             curr.execute(sql,username)
             output=curr.fetchone()
-            # print(output)
+            # print(output[0])
+            # print(output[0]," hheehe ",password)
+            # output = output[0].decode('utf-8')
             if(output):
-                if(password == output[0]):
+                temp = output[0]
+                temp = bytes(temp,encoding='utf-8')
+                temp = crypto.decode(temp)
+                if(password == temp):
                     return 1 #"correctPassword"
                 else: return 0#"wrongPassword"    
             else:
                 return -1 #"UsernameDosenotExit"    
     except Exception as e:
         print(e)       
-
+        
 def ngo_signup(ngo_id,ngo_name,username,password,email,phone_no):
     conn=pymysql.connect(
         host=credential.host,
@@ -80,3 +87,21 @@ def ngo_login(username,password):
                 return -1 #"UsernameDosenotExit"    
     except Exception as e:
         print(e)       
+
+def user_check(user_id):
+    conn=pymysql.connect(
+        host=credential.host,
+        port=credential.port,
+        user=credential.user,
+        password=credential.password,
+        db=credential.databasename
+    )
+    try:
+        with conn.cursor() as curr:
+            sql = "select exists(select 1 from user_master where user_id=(%s))"
+            curr.execute(sql,(user_id))
+            output = curr.fetchone()
+            return output[0]
+    except Exception as e:
+        print(e)   
+# print(user_check("hhhdhhd"))
